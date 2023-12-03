d29cf06ce55bee2e8299c9e113b956cf9e86ca1a
tomirisdadamurzaeva <tomiris.db@mail.ru>
2023-12-03 18:36:28 +0800

    Complete prac 5 state names exercise

--- state_names.py
+++ state_names.py
@@ -5,14 +5,19 @@ File needs reformatting
 """
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD":"Queensland", "NSW": "New South Wales", "NT" : "Northern Territory", "WA" : "Western Australia",
-            "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
+for code, name in CODE_TO_NAME.items():
+    print(f"{code} is {name}")
 
-state_code = input("Enter short state: ")
-while state_code != "":
-    if state_code in CODE_TO_NAME:
+while True:
+    try:
+        state_code = input("Enter short state: ").upper()
+        if not state_code:
+            break
         print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+
+    except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+
